04b7320d08bbb07ab239d40a163d3d28c1aa63bb
Timofakar <timofeykarnakovka@gmail.com>
2023-01-23 18:00:46 +0700

    появились первые работ по методичке

---
+++ сишарп1диалог/CodeFile1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+class Enteringinteger
+{
+    static void Main()
+    {
+        // Текстовые переменные:
+        string res, txt, gas;
+        // Целочисленные переменные:
+        int sum, notsum, born,gorn;
+        // Отображение окна с полем ввода:
+        res = Interaction.InputBox("Первое число");
+        gas= Interaction.InputBox("Второе число");
+        // Преобразование текста в число:
+        born = Int32.Parse(res);
+        gorn = Int32.Parse(gas);
+        notsum =   born-gorn;
+        sum = born + gorn;
+        txt = "Сумма = " + sum + "\n А разность = "+notsum;
+        // Окно с сообщением:
+        MessageBox.Show(txt, "Результат");
+    }
+}
---
+++ сишарп1конса/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+namespace ConsoleApp1
+{
+    
+    class InputConsoleDemo
+    {
+        static void Main()
+        {
+            // Текстовая переменная:
+            string name, gon;
+            int born, gorn, sum, notsum;
+            // Заголовок консольного окна:
+            Console.Title = "Сбор данных";
+            // Сообщение в консоли:
+            Console.Write("Первое число : ");
+            // Считывание текста:
+            name = Console.ReadLine();
+            born = Int32.Parse(name);
+            Console.Title = "Сбор данных";
+            // Сообщение в консоли:
+            Console.Write("Второе число : ");
+            // Считывание текста:
+            gon = Console.ReadLine();
+            gorn = Int32.Parse(gon);
+            sum = born + gorn;
+            notsum = born - gorn;
+            // Еще одна текстовая переменная:
+            string tхt = "Cумма =  " + sum + "\nа разность = "+notsum;
+            // Заголовок консольного окна:
+            Console.Title = "Результат";
+            // Сообщение в консоли:
+            Console.WriteLine(tхt);
+            Console.ReadKey();
+        }
+    }
+        }
